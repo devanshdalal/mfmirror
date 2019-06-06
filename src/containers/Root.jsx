418ff7aa7f24e6329bd66f1608c6c1f953756ecd
manish-dalal <md4640@gmail.com>
2019-06-06 22:45:53 +0530

    Added basic npms

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 class Root extends React.Component {
-    render(){
-        return <div></div>
-    }
+  render() {
+    return <div>hello</div>;
+  }
 }
 
 export default Root;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/Root';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-virtualized/styles.css";
+import "./index.css";
+import App from "./containers/Root";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
