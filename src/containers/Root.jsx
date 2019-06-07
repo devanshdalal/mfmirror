cc20953a590c6f8310ca2837f8c3592c1fd7ce77
manish-dalal <md4640@gmail.com>
2019-06-07 09:30:09 +0530

    Added table in form component

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
+import Form from "./home/Form";
 
 class Root extends React.Component {
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        <Route path="/" exact component={Form} />
+      </div>
+    );
   }
 }
 
