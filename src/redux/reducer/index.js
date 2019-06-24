4eef40cdf146d64d863ce90c7ab3522254ca3efd
manish-dalal <manish.dalal@daffodilsw.com>
2019-06-24 09:53:14 +0530

    reducer update

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,15 +1,5 @@
 import { combineReducers } from "redux";
-import { UPDATE_LOADING } from "../constants/action-types";
-
-const initialState = { loadState: false };
-const loading = (state = initialState, action) => {
-  switch (action.type) {
-    case UPDATE_LOADING:
-      return { loadState: action.payload };
-    default:
-      return state;
-  }
-};
+import loading from "./loading";
 
 const rootReducer = combineReducers({ loading });
 
