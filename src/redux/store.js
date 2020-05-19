ee3d97d67ade3f17c067a0146367e8eb62eae149
Devansh Dalal <devansh.dalal@thoughtspot.com>
2020-05-19 13:04:21 +0530

    fix redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,8 +8,8 @@ const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   loadingReducer,
   compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(sagaMiddleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 sagaMiddleware.run(rootsaga);
