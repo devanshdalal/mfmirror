9f66637ea8fa954a5ad8401daed05d8e28c64df5
Devansh Dalal <mr.devanshdalal@gmail.com>
2021-02-23 22:54:38 +0530

    Manual deploy

---
+++ precache-manifest.992adbb020e4c5fdcd8c91cb252cf4aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca7af0d47bff75a861635fb614fad91",
+    "url": "/mfmirror/index.html"
+  },
+  {
+    "revision": "07c483fb40417b1f9359",
+    "url": "/mfmirror/static/css/2.bdedae93.chunk.css"
+  },
+  {
+    "revision": "4a0cf82cbe2c7c0bcb92",
+    "url": "/mfmirror/static/css/main.4e01c097.chunk.css"
+  },
+  {
+    "revision": "07c483fb40417b1f9359",
+    "url": "/mfmirror/static/js/2.8a51e80d.chunk.js"
+  },
+  {
+    "revision": "0596ad1725aca3d0431d00af27753669",
+    "url": "/mfmirror/static/js/2.8a51e80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0cf82cbe2c7c0bcb92",
+    "url": "/mfmirror/static/js/main.8071b95d.chunk.js"
+  },
+  {
+    "revision": "bf93cff05abea9c0b6cc",
+    "url": "/mfmirror/static/js/runtime-main.306aa549.js"
+  },
+  {
+    "revision": "c24dc86f663ffee1c844595aac8046e7",
+    "url": "/mfmirror/static/media/loading-spinner.c24dc86f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mfmirror/precache-manifest.9237e0421642721146836d5bf34e3824.js"
+  "/mfmirror/precache-manifest.992adbb020e4c5fdcd8c91cb252cf4aa.js"
 );
 
 self.addEventListener('message', (event) => {
